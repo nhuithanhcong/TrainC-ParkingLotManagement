4509f66a5d160e92d219911b55540f399f36a256
nhuithanhcong <pquan2212@gmail.com>
2026-04-20 20:45:46 +0700

    first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <>
+
+	int main(){
+		
+	int choice;
+    while(1) {
+        printf("\n--- PARKING MANAGEMENT SYSTEM ---");
+		//menu
+        printf("\nChoice: ");
+        scanf("%d", &choice);
+
+	/*  
+		switch(choice) {
+            case 1:
+            case 2: 
+            case 3: 
+            case 4: 
+                exit(0);
+        }
+    }
+	*/
+    return 0;
+}
