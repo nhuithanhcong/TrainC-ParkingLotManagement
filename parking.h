44901f8ac0ec5f1e682b389587dde58b11c8c61a
doananhtran242007-rgb <doananhtran242007@gmail.com>
2026-04-25 17:36:49 +0700

    update parking.c and parking.h

--- parking.h
+++ parking.h
@@ -1,6 +1,14 @@
-/*
-#ifndef 
-#define
+#ifndef PARKING_H
+#define PARKING_H
+
+#include <time.h>
+#include <string.h>
+int addlicensePlate(char licensePlate[3636][16],int n,int vehicleType[3636],time_t entryTime[3636]); //Thêm biển số
+
+int isDuplicateLicensePlate(char licensePlate[][16], int n, char tempPlate[]); //Kiểm tra trùng biển số
+
+char findlicensePlate(char licensePlate[3636][16], int n); //Tìm biển số
+
+int display(char licensePlate[3636][16], int n,int vehicleType[3636],time_t entryTime[3636]);      // Hiển thị danh sách
 
 #endif
-*/
