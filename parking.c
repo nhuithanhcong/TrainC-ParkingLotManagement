90b04d43b0f8484dae29f35491ce9ba13de5dedf
doananhtran242007-rgb <doananhtran242007@gmail.com>
2026-04-27 11:39:33 +0700

    DƠNE(2)

--- parking.c
+++ parking.c
@@ -3,86 +3,78 @@
 #include <time.h>
 #include <string.h>
 #include "parking.h"
-char findlicensePlate(char licensePlate[3636][16], int n){
+#include "utils.h"
+
+
+int findlicensePlate(char licensePlate[3636][16],int n,int vehicleType[3636],time_t entryTime[3636],int status[3636]){
 	char find[16];
-	printf("Find license plate:\n");
+	int found=0;
+	printf("Find license plate:");
 	scanf("%15s",find);
 	for(int i=0;i< n;i++){
 		if(strcmp(find,licensePlate[i])==0){   //strcmp: ss 2 chuỗi find và license, giống thì = 0
-			printf("Existed");
-			break; 
+			found=1;
+			printf("Existed\n");
+			printf("Type of vehicle: %d\n",vehicleType[i]); 
+			printf("License Plate: %s\n",licensePlate[i]);
+			printf("Entry Time: %s",ctime(&entryTime[i]));
+			printf("Status: %d\n",status[i]);
+			break;
 	}}
-	printf("Not found");
+	if(found==0){
+			printf("Not found");
+	}
 	return 0;		
 	}
 	
-	
-	
-int isDuplicatelicensePlate(char licensePlate[3636][16],int n, char tempPlate[]){
-	for(int i=0;i<n;i++){
-		if (strcmp(tempPlate,licensePlate[i])==0){
-			return 1;
-		}
-	}
-	return 0;
-}
-
-
-
-int addlicensePlate(char licensePlate[3636][16],int n,int vehicleType[3636],time_t entryTime[3636]){
+int addlicensePlate(char licensePlate[3636][16],int n,int vehicleType[3636],time_t entryTime[3636],int status[3636]){
 	char tempPlate[16];
-	for(int i=0; i<n ; i++){
-		printf("Enter license plate:");
-		scanf("%15s",tempPlate);
-		if(isDuplicatelicensePlate(licensePlate,n,tempPlate)){
-			printf("Duplicate plate! Try again.\n");
-			i--;
-			continue;
+	int count =0;
+	while(count<n){
+	 	while(1){
+			printf("Enter license plate:");
+			scanf("%15s",tempPlate);
+				if(isDuplicatelicensePlate(licensePlate,count,tempPlate)){
+					printf("Duplicate plate! Try again.\n");
 		}
-		strcpy(licensePlate[i], tempPlate); // Copy nội dung từ tempPlate(biến tạm) qua licensePlate[i]
-		printf("Add successful!\n");
-		printf("---Menu---\n 0:motorbycle\n 1:car\n 2:truck\n");
-		do{
-			printf("Enter type of vehicle:");
-			scanf("%d",&vehicleType[i]);
-		if(vehicleType[i]!=0 && vehicleType[i]!=1 && vehicleType[i]!=2){
-			printf("Invalid value! Please try again\n");
+				else{ break;}
+		}
+			strcpy(licensePlate[count], tempPlate); // Copy nội dung từ tempPlate(biến tạm) qua licensePlate[i]
+			printf("Add successful!\n");
+			printf("------Menu------\n Type of vehicle\n 0:motorbycle\n 1:car\n 2:truck\n ..................\n Status\n 0:in\n 1:out\n");
+			do{ printf("Enter type of vehicle:");
+				scanf("%d",&vehicleType[count]);
+				if(vehicleType[count]!=0 && vehicleType[count]!=1 && vehicleType[count]!=2){
+					printf("Invalid value! Please try again\n");
 			}
-		}while(vehicleType[i]!=0 && vehicleType[i]!=1 && vehicleType[i]!=2);
-		printf("Add successful!\n");
-		entryTime[i] = time(NULL);                  // lưu thời gian hiện tại (dạng số)
-		printf("%s", ctime(&entryTime[i]));         // in ra dạng người đọc được
-	
-	return 0;
-}}
-
-
-
-int display(char licensePlate[3636][16], int n,int vehicleType[3636],time_t entryTime[3636]){
+		}	while(vehicleType[count]!=0 && vehicleType[count]!=1 && vehicleType[count]!=2);
+				printf("Add successful!\n");
+		do {printf("Status:");
+			scanf("%d",&status[count]);
+			if (status[count]!=0 && status[count]!=1){
+				printf("Invalid value! Please try again\n");
+			}
+		}
+		while(status[count]!=0 && status[count]!=1);
+			printf("Add successful!\n");
+		entryTime[count] = time(NULL);                  // lưu thời gian hiện tại (dạng số)
+		printf("%s", ctime(&entryTime[count]));         // in ra dạng người đọc được
+		count++;
+}
+	return count;
+}
+int display(char licensePlate[3636][16], int n,int vehicleType[3636],time_t entryTime[3636], int status[3636]){
 	printf("=========== List of Vehicle =========== \n");
-	for (int i=0; i<n;i++){
-		entryTime[i] = time(NULL);
+	for(int i= 0; i<n; i++){
+	
 		printf("Type of vehicle: %d\n",vehicleType[i]); 
 		printf("License Plate: %s\n",licensePlate[i]);
+		printf("Status: %d\n",status[i]);
 		printf("Entry Time: %s",ctime(&entryTime[i]));
-	}
+	}                                         
 	printf("========================================\n");	
-	}
-	
-	
-	
-int main (){
-	char licensePlate[3636][16]; //Tối đa 3636 xe và mỗi biển số dưới 16 ký tự
-	int n;
-	time_t entryTime[3636];
-	int vehicleType[3636];
-	printf("Enter number of vehicles to add to the list:");
-	scanf("%d",&n);
-	addlicensePlate(licensePlate,n,vehicleType,entryTime); 	
-	display(licensePlate,n,vehicleType,entryTime);
-	findlicensePlate(licensePlate,n);	
-	return 0;
-}
+	}	
+
 
 
 
--- parking.h
+++ parking.h
@@ -3,12 +3,10 @@
 
 #include <time.h>
 #include <string.h>
-int addlicensePlate(char licensePlate[3636][16],int n,int vehicleType[3636],time_t entryTime[3636]); //Thêm biển số
+int addlicensePlate(char licensePlate[3636][16],int n,int vehicleType[3636],time_t entryTime[3636], int status[3636]); //Thêm biển số
 
-int isDuplicateLicensePlate(char licensePlate[][16], int n, char tempPlate[]); //Kiểm tra trùng biển số
+int findlicensePlate(char licensePlate[3636][16],int n,int vehicleType[3636],time_t entryTime[3636],int status[3636]); //Tìm biển số
 
-char findlicensePlate(char licensePlate[3636][16], int n); //Tìm biển số
-
-int display(char licensePlate[3636][16], int n,int vehicleType[3636],time_t entryTime[3636]);      // Hiển thị danh sách
+int display(char licensePlate[3636][16], int n,int vehicleType[3636],time_t entryTime[3636], int status[3636]);      // Hiển thị danh sách
 
 #endif
