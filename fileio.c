42c7106407c03220fcdf5009d0628c6ab58a6750
donghao2209-ship-it <donghao2209@gmail.com>
2026-05-09 22:01:36 +0700

    Update fileio.c

--- fileio.c
+++ fileio.c
@@ -12,12 +12,12 @@ void saveToFile(Vehicle list[], int count){
 // count la bien dem so luong xe hien tai trong bai do xe. (Do khong biet ham main dung bien ten gi de dem so luong xe nen tam thoi de bien
 // nay)
 for (int i = 0; i < count; i++){
-	fprintf(f, "%s, %d, %ld, %ld, %.2f, %d\n", //luu cac du lieu cach nhau bang dau phay
+	fprintf(f, "%s, %ld, %ld, %.2f, %d, %d\n", //luu cac du lieu cach nhau bang dau phay
 	        list[i].licensePlate,
-	        list[i].vehicleType,
 	        (long)list[i].entryTime,
 	        (long)list[i].exitTime,
 	        list[i].fee,
+		    list[i].vehicleType,
 	        list[i].status);
 }
 fclose(f);
@@ -33,17 +33,17 @@ int loadFromFile ( struct Vehicle list[] ){
 	}
 	int count = 0; //Bien count nay dung de dem so luong xe trong ham nay (bien nay ko lien quan den các ham khac)
 	// ham while duoi giup doc tung dong du lieu trong file de dem so luong xe cho bien count cho den het file (EOF)
-	while (fscanf(f, "%[^,], %d, %ld, %ld, %lf, %d\n",
+	while (fscanf(f, "%[^,], %ld, %ld, %lf, %d, %d\n",
 	        list[i].licensePlate,
-	        &list[i].vehicleType,
 	        &list[i].entryTime,
 	        &list[i].exitTime,
 	        &list[i].fee,
+		    &list[i].vehicleType,
 	        &list[i].status) != EOF){
 		count++;
 	// Gioi han suc chua cua bai xe
-	if (count >= 3636){
-		break;}    	
+		if (count >= 3636){
+			break;}    	
 	}
 	fclose(f);
 	printf("Load the data of %d vehicles.\n", count);
