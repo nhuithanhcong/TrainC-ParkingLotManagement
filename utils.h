b8ce1c4c3f7e3824a7e3b5104bfda227a8e4dbd2
nhuithanhcong <pquan2212@gmail.com>
2026-05-10 22:17:27 +0700

    Refactor utils.h to define Vehicle and PriceConfig structs

--- utils.h
+++ utils.h
@@ -1,10 +1,29 @@
+#ifndef UTILS_H
+#define UTILS_H
 
-#ifndef 
-#define
-typedef struct{
-int vehicleType;
-char typeName[30];
-double pricePerHour;
-double minimumFee;
-}
+#include <time.h>
+
+
+typedef struct {
+    char licensePlate[16];
+    time_t entryTime;
+    time_t exitTime;  
+    double fee;
+    int vehicleType;       
+    int status;      
+} Vehicle;
+
+
+typedef struct {
+    char typeName[16];
+    double pricePerHour;
+    double minimumFee;
+    int vehicleType;
+} PriceConfig;
+
+
+void clearBuffer();
+void printTime(time_t t);
+int findVehicleIndex(Vehicle *vehicles, int count, const char *plate);
+
 #endif
